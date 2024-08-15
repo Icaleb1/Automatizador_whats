da5c7db74f237cb7e2d613441b18b215d90f4fba
Icaleb1 <calebfernandes080@gamil.com>
2024-08-14 23:27:47 -0300

    Correções na organização do código

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-"""
-automatizar essa planilha com PYTHON e 
-assimilar a EVO para enviar as mensagens automaticamente para os clientes
-
-"""
-
-#Descrever passos manuais e transformar em código
-#Ler planilha e guardar informações
-#Criar links personalizados do whats e enviar MSGs
-#Com base nos dados da planilha
-
 import pandas as pd
 import openpyxl
 from openpyxl.styles import PatternFill
@@ -24,10 +13,46 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import traceback
 
-
-
 root = tk.Tk()
 root.withdraw()
+
+def logErro(mensagem):
+    with open('log_erros.txt', 'a') as log_file:
+        log_file.write(f'{mensagem}\n')
+
+def carregarArquivo():
+    arquivoSelecionado = filedialog.askopenfilename(
+        title="Selecione o arquivo Excel",
+        filetypes=[("Excel files", "*.xlsx *.xls")]
+    )
+    if not arquivoSelecionado:
+        logErro('Nenhum arquivo selecionado. Encerrando o programa.')
+        sys.exit()
+    return arquivoSelecionado
+
+def obterNumMensagens():
+    while True:
+        try:
+            numMensagens = simpledialog.askinteger('Quantidade de mensagens ', 'Quantas mensagens deseja enviar? ')
+            if numMensagens is None:
+                logErro('Nenhum número válido foi inserido!')
+                sys.exit()
+            if numMensagens <= 0:
+                raise ValueError('O número de mensagens deve ser positivo.')
+            break
+        except ValueError as e:
+            logErro(f'Valor inválido: {e}. Por favor, insira um número inteiro positivo.')
+    return numMensagens
+
+def obterMensagens(numMensagens):
+    mensagens = []
+    for i in range(numMensagens):
+        mensagem = simpledialog.askstring('Mensagem', f'Digite a mensagem {i+1}:')
+        if mensagem is None:
+            logErro('Operação cancelada pelo usuário.')
+            sys.exit()
+        mensagens.append(mensagem)
+    return mensagens
 
 def resource_path(relative_path):
     try:
@@ -36,96 +61,77 @@ def resource_path(relative_path):
         base_path = os.path.abspath('.')
     return os.path.join(base_path, relative_path)
 
-image_path = resource_path("botao.png")
+def enviarMensagens(navegador, mensagens, paginaClientes):
+    linhaVerde = PatternFill(start_color='00FF00', end_color='00FF00', fill_type='solid')
+    linhaVermelha = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
 
-file_path = filedialog.askopenfilename(
-    title="Selecione o arquivo Excel",
-    filetypes=[("Excel files", "*.xlsx *.xls")]
-)
+    for linha in paginaClientes.iter_rows(min_row=2):
+        matricula = linha[0].value  
+        nome = linha[1].value
+        telefone = linha[2].value
+        email = linha[3].value
+        valor_debito = linha[4].value
 
-if not file_path:
-    messagebox.showerror('Error', 'nenhum arquivo selecionado. Encerrando o programa.')
-    sys.exit()
+        if nome is None or telefone is None:
+            continue
 
-while True:
-    try:
-        num_mensagens = simpledialog.askinteger('Quantidade de mensagens ', 'Quantas mensagens deseja enviar? ')
-        if num_mensagens is None:
-            messagebox.showwarning('Atenção!', 'Digite um número válido!')
-            sys.exit()
-        if num_mensagens <= 0:
-            raise ValueError('O número de mensagens deve ser positivo.')
-        break
-    except ValueError as e:
-        messagebox.showerror('Erro', f'Valor inválido: {e}. Por favor, insira um número inteiro positivo.')
+        primeiraMensagem = f"Olá, {nome}\n\n{mensagens[0]}"
 
-mensagens = []
+        for i, mensagem in enumerate(mensagens): 
+            if i == 0:
+                mensagemAtual = primeiraMensagem
+            else:
+                mensagemAtual = mensagem
 
-for i in range(num_mensagens):
-    mensagem = simpledialog.askstring('Mensagem', f'Digite a mensagem {i+1}:')
-    if mensagem is None:
-        messagebox.showwarning('Atenção', 'Operação cancelada.')
-        sys.exit()
-    mensagens.append(mensagem)
+            try:
+                linkMensagemWhats = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagemAtual)}'
+                navegador.get(linkMensagemWhats)
 
-options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=/path/to/your/custom/profile")  # Use o caminho para o perfil do usuário
-navegador = webdriver.Chrome(options=options)
+                while len(navegador.find_elements(by='id', value='side')) < 1:
+                    sleep(10)
 
-navegador.get("https://web.whatsapp.com/")
+                navegador.find_element(by='xpath', value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p').send_keys(Keys.ENTER)
+                sleep(15)
 
-while len(navegador.find_elements(by='id', value='side')) < 1:
-    sleep(5)
+                navegador.switch_to.window(navegador.window_handles[0])
 
-workbook = openpyxl.load_workbook(file_path)
-pagina_clientes = workbook['Planilha1']
-
-green_fill = PatternFill(start_color='00FF00', end_color='00FF00', fill_type='solid')
-red_fill = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
-
-for linha in pagina_clientes.iter_rows(min_row=2):
-    matricula = linha[0].value  
-    nome = linha[1].value
-    telefone = linha[2].value
-    email = linha[3].value
-
-    valor_debito = linha[4].value
-
-    if nome is None or telefone is None:
-        continue
-
-    primeira_mensagem = f"Olá, {nome}\n\n{mensagens[0]}"
-
-    for i, mensagem in enumerate(mensagens): 
-
-        if i == 0:
-            mensagem_atual = primeira_mensagem
-        else:
-            mensagem_atual = mensagem
-
-        try:
-            link_mensagem_whats = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem_atual)}'
-            navegador.get(link_mensagem_whats)
-
-            while len(navegador.find_elements(by='id', value='side')) < 1:
                 sleep(10)
 
-            navegador.find_element(by='xpath', value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p').send_keys(Keys.ENTER)
-            sleep(15)
+                for cell in linha:
+                    cell.fill = linhaVerde
 
-            navegador.switch_to.window(navegador.window_handles[0])
+            except Exception as e:
+                logErro(f'Não foi possível enviar mensagem para {nome}, ({telefone}). Erro: {e}')
+                for cell in linha:
+                    cell.fill = linhaVermelha
 
-            sleep(10)
+try:
+    arquivoSelecionado = carregarArquivo()
 
-            for cell in linha:
-                cell.fill = green_fill
+    numMensagens = obterNumMensagens()
 
-        except Exception as e:
-            messagebox.showerror('Erro', f'Não foi possível enviar mensagem para {nome}, ({telefone}). Detalhes do erro foram registrados.')
+    mensagens = obterMensagens(numMensagens)
 
-            for cell in linha:
-                cell.fill = red_fill
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=/path/to/your/custom/profile")
+    navegador = webdriver.Chrome(options=options)
 
-workbook.save(file_path)
+    navegador.get("https://web.whatsapp.com/")
 
-messagebox.showinfo('Concluído', 'Mensagens enviadas.')
+    while len(navegador.find_elements(by='id', value='side')) < 1:
+        sleep(5)
+
+    workbook = openpyxl.load_workbook(arquivoSelecionado)
+    paginaClientes = workbook['Planilha1']
+
+    enviarMensagens(navegador, mensagens, paginaClientes)
+
+    workbook.save(arquivoSelecionado)
+
+    messagebox.showinfo('Concluído', 'Mensagens enviadas.')
+
+except Exception as e:
+   logErro(f'Ocorreu um erro inesperado: {traceback.format_exc()}')
+
+finally:
+    navegador.quit()
--- app.spec
+++ app.spec
@@ -5,7 +5,7 @@ a = Analysis(
     ['app.py'],
     pathex=[],
     binaries=[],
-    datas=[('botao.png', '.')],
+    datas=[],
     hiddenimports=[],
     hookspath=[],
     hooksconfig={},
